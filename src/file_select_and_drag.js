5096388857238da2b365ccc2ee3589224bd5c220
zyxia <969919860@qq.com>
2017-05-28 02:11:52 +0800

    界面跟随鼠标移动成功

---
+++ src/file_select_and_drag.js
@@ -0,0 +1,10 @@
+var fileSelectonMouseMove = function()
+{
+    var x = event.x;
+    var y = event.y;
+    var file_view = document.getElementById("file_select_frame"); 
+    file_view.style["margin-left"] =  x;
+    file_view.style["margin-top"] = y;     
+}
+
+ 
